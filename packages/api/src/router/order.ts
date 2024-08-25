23d93c86bb4464c7d593697bddae74d9e5ddc235
thadtayo <thadtayo@gmail.com>
2024-08-25 14:28:15 -0400

    comment to remove

--- packages/api/src/router/order.ts
+++ packages/api/src/router/order.ts
@@ -7,6 +7,7 @@ export const orderRouter = {
     return ctx.db.order.findMany();
   }),
   unprotectedGetAllOrders: publicProcedure.query(({ ctx }) => {
+    // TODO: REMOVE
     return ctx.db.order.findMany();
   }),
 } satisfies TRPCRouterRecord;
