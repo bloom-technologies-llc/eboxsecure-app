6eb9fcd803837216f19b2a59b1028e39786bc5dd
thadtayo <thadtayo@gmail.com>
2024-08-28 12:32:09 -0400

    previous commit + this commit is 0.1.28. 0.1.27 works properly

--- apps/client-mobile/app.config.ts
+++ apps/client-mobile/app.config.ts
@@ -8,7 +8,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   name: "ebox-client-mobile",
   slug: "ebox-client-mobile",
   scheme: "expo",
-  version: "0.1.27",
+  version: "0.1.28",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "automatic",
