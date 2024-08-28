e0ae4a45d08b8deb7fb951e4e7484822f0718fef
thadtayo <thadtayo@gmail.com>
2024-08-28 13:06:58 -0400

    0.1.28 failed; upgrading clerk for 0.1.29

--- apps/client-mobile/app.config.ts
+++ apps/client-mobile/app.config.ts
@@ -8,7 +8,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   name: "ebox-client-mobile",
   slug: "ebox-client-mobile",
   scheme: "expo",
-  version: "0.1.28",
+  version: "0.1.29",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "automatic",
