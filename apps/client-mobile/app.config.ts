96b247e23285062a5e9b5378eb03e35a9b740c71
thadtayo <thadtayo@gmail.com>
2024-08-29 08:14:56 -0400

    0.1.36 failed. 0.1.37 dropping expo version

--- apps/client-mobile/app.config.ts
+++ apps/client-mobile/app.config.ts
@@ -8,7 +8,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   name: "ebox-client-mobile",
   slug: "ebox-client-mobile",
   scheme: "expo",
-  version: "0.1.36",
+  version: "0.1.37",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "automatic",
