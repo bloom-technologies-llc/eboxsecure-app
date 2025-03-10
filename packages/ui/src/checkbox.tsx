c2848e70f0df2e05c5bea257af6b9a30716e3381
Alan <zeyu.weng@gmail.com>
2025-03-10 17:57:20 -0400

    addressed prev pr concerns

--- packages/ui/src/checkbox.tsx
+++ packages/ui/src/checkbox.tsx
@@ -2,8 +2,9 @@
 
 import * as React from "react";
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
-import { cn } from "src/lib/utils";
 import { CheckIcon } from "@radix-ui/react-icons";
+
+import { cn } from "./lib/utils";
 
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
