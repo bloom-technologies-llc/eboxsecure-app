cf1de03bd7c62aa182c60b710974c579caf0fe05
Alan <zeyu.weng@gmail.com>
2025-04-09 16:19:41 -0400

    small changes

--- apps/admin-portal/src/app/(home)/locations/location-details/[locationId]/page.tsx
+++ apps/admin-portal/src/app/(home)/locations/location-details/[locationId]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { useParams, useRouter, useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
   ArrowLeft,
