73602d32ec9a3aa47e180616f9c688683d55c17e
thadtayo <josh_tayo@yahoo.com>
2025-06-07 03:41:48 -0400

    failed attempts

--- apps/client-mobile/app.config.ts
+++ apps/client-mobile/app.config.ts
@@ -8,7 +8,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   name: "EboxSecure",
   slug: "ebox-client-mobile",
   scheme: "expo",
-  version: "0.3.9",
+  version: "0.3.12",
   orientation: "portrait",
   icon: "./assets/images/logos/eboxsecure-logo-ios.png",
   userInterfaceStyle: "automatic",
