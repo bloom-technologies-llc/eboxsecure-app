0706515d40c2d9bd8666012be8217c16100ecd1a
thadtayo <josh_tayo@yahoo.com>
2025-07-29 22:29:46 -0400

    adding stripe webhooks

---
+++ apps/client-web/src/lib/sync-customer-data.ts
@@ -0,0 +1,41 @@
+import "server-only";
+
+import { env } from "@/env";
+import { SubscriptionData } from "@/types/subscription";
+import Stripe from "stripe";
+
+import { kv } from "@ebox/redis-client";
+
+export async function syncCustomerData(customerId: string) {
+  const stripe = new Stripe(env.STRIPE_SECRET_KEY);
+
+  const subscriptions = await stripe.subscriptions.list({
+    customer: customerId,
+    limit: 1,
+    status: "all",
+    expand: ["data.default_payment_method"],
+  });
+
+  if (subscriptions.data.length === 0) {
+    const subData = { status: "none" };
+    await kv.set(`stripe:customer:${customerId}`, subData);
+    return subData;
+  }
+
+  const subscription = subscriptions.data[0];
+
+  if (!subscription) {
+    throw new Error(`No subscriptions found`);
+  }
+
+  const subData: SubscriptionData = {
+    subscriptionId: subscription.id,
+    status: subscription.status,
+    priceIds: subscription.items.data.map((item) => item.price.id),
+    currentPeriodEnd: subscription.items.data[0]?.current_period_end!,
+    currentPeriodStart: subscription.items.data[0]?.current_period_start!,
+    cancelAtPeriodEnd: subscription.cancel_at_period_end,
+  };
+  await kv.set(`stripe:customer:${customerId}`, subData);
+  return subData;
+}
---
+++ apps/client-web/src/types/subscription.ts
@@ -0,0 +1,18 @@
+// Must be equal to the SubscriptionType enum in the client-api package
+export type SubscriptionData = {
+  subscriptionId: string;
+  status:
+    | "active"
+    | "canceled"
+    | "past_due"
+    | "unpaid"
+    | "incomplete"
+    | "incomplete_expired"
+    | "trialing"
+    | "paused"
+    | "none";
+  priceIds: string[];
+  currentPeriodEnd: number;
+  currentPeriodStart: number;
+  cancelAtPeriodEnd: boolean;
+};
