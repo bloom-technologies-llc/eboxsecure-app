5d59aa026047a38e104b97cb58202fbfd39e97c5
thadtayo <josh_tayo@yahoo.com>
2025-07-30 01:31:45 -0400

    redirect to payment on onboarding

--- apps/client-web/src/app/onboarding/page.tsx
+++ apps/client-web/src/app/onboarding/page.tsx
@@ -1,9 +1,16 @@
 import { redirect } from "next/navigation";
+import { hasValidSubscription } from "@/lib/stripe";
 import { api } from "@/trpc/server";
 
 import PortraitPhotoUpload from "./PortraitPhotoUpload";
 
 export default async function Page() {
+  const userHasValidSubscription = await hasValidSubscription();
+
+  if (!userHasValidSubscription) {
+    redirect("/payment");
+  }
+
   const isOnboarded = await api.onboarding.isOnboarded();
   if (isOnboarded) {
     redirect("/");
